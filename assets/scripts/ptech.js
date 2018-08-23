6ec00f542dc1498f0bce5dcee1411418d97718a9
pensadotech <armando@pensadotech.com>
2018-08-22 21:45:43 -0700

    Initial idea for Portfolio cards

---
+++ assets/scripts/ptech.js
@@ -0,0 +1,13 @@
+//  Pensadotech JavaScripts
+
+$(document).ready(function(){
+   
+    console.log("Pensadotech GitHub pages");
+    
+    var audio = new Audio("../sounds/selectSound.mp3");
+
+    // TODO: add click selected sound
+    
+    audio.play();
+
+}); // Document ready

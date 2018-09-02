08772750fcbc3aa09c2d09dbf69ae601e1047cba
pensadotech <armando@pensadotech.com>
2018-09-01 17:23:14 -0700

    Add contents and adjust portfolio

--- assets/scripts/ptech.js
+++ assets/scripts/ptech.js
@@ -1,13 +1,2 @@
 //  Pensadotech JavaScripts
 
-$(document).ready(function(){
-   
-    console.log("Pensadotech GitHub pages");
-    
-    var audio = new Audio("../sounds/selectSound.mp3");
-
-    // TODO: add click selected sound
-    
-    audio.play();
-
-}); // Document ready
